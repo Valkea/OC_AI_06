0f5f9bb2b68d7f283c6b766b4df3f845ed81d41b
Emmanuel <github-sub@shedge.com>
2022-07-01 16:23:49 +0200

    feature: now collect reviews with rating 1 or 2 only (and optionally all ratings)
    refactor: add docstrings

--- 02_API_Scrapping.py
+++ 02_API_Scrapping.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import os
 import requests
 import argparse
@@ -14,13 +13,41 @@ from utils import progress_monitor
 with open("secrets.txt") as f:
     API_KEY = f.readline().strip()
 
+##################################################
+# Functions collecting data from Yelp API
+##################################################
 
-def get_business(search, limit=50, offset=0, verbose=0):
-    """TODO"""
+
+def get_business(location, limit=50, offset=0, verbose=0):
+    """
+    This function connect the Yelp API and gather informations
+    about businesses given the provided location parameter.
+
+    Parameters
+    ----------
+    location: str
+         The geographic area to be used when searching for businesses
+    limit: int
+        Number of business results to return
+        (50 is the default AND maximum value)
+    offset: int
+        Offset the list of returned business results by this amount
+    verbose: int
+        0 -> Silent / Progress bar
+        1 -> Infos about the downloaded reviews
+        2 -> HTTP status
+
+    Returns
+    -------
+    dictionary:
+        The request result
+    int:
+        The updated offset
+    """
 
     url = "https://api.yelp.com/v3/businesses/search"
     query = {
-        "location": search,
+        "location": location,
         "limit": limit,
         "offset": offset,
     }
@@ -33,7 +60,27 @@ def get_business(search, limit=50, offset=0, verbose=0):
 
 
 def get_business_id(request, limit, business_ids, verbose=0):
-    """TODO"""
+    """
+    Parse the businesses JSON collected with 'get_business()'
+
+    Parameters
+    ----------
+    request: dictionnary
+        A dictionary containing the businesses informations
+    limit: int
+        The number of business results to parse
+    business_ids: list
+        An external list used to collect the business ids
+    verbose: int
+        0 -> Silent / Progress bar
+        1 -> Infos about the downloaded reviews
+        2 -> HTTP status
+
+    Returns
+    -------
+    It won't return anything, but the business_ids is updated
+    internally, and hence externaly as this is a reference.
+    """
 
     json = request.json()
 
@@ -48,9 +95,37 @@ def get_business_id(request, limit, business_ids, verbose=0):
             raise Exception
 
 
-def get_business_reviews(id, reviews, max_reviews, verbose=0):
-    """TODO"""
+def get_business_reviews(id, reviews, max_reviews, collect_all=False, verbose=0):
+    """
+    This function connect the Yelp API and gather REVIEWS from
+    the provided business ID.
 
+    Parameters
+    ----------
+    id: int
+        Unique Yelp ID of the business we want to get reviews from.
+    reviews: list
+        An external list used to collect the reviews
+    max_reviews: int
+        Number of business reviews to return
+        (the maximum value is 200)
+    collect_all: bool
+        Whether or not the script should collet all ratings
+    verbose: int
+        0 -> Silent / Progress bar
+        1 -> Infos about the downloaded reviews
+        2 -> HTTP status
+
+    Returns
+    -------
+    It won't return anything, but the reviews is updated
+    internally, and hence externaly as this is a reference.
+    """
+
+    if collect_all:
+        selected_stars = [1,2,3,4,5]
+    else:
+        selected_stars = [1,2]
     url = f"https://api.yelp.com/v3/businesses/{id}/reviews"
     headers = {"Authorization": f"Bearer {API_KEY}"}
     r = requests.get(url, headers=headers)
@@ -62,6 +137,10 @@ def get_business_reviews(id, reviews, max_reviews, verbose=0):
     for review in json["reviews"]:
         text = review["text"]
         rate = review["rating"]
+
+        if rate not in selected_stars:
+            continue
+
         reviews.append({"text": text, "rating": rate})
 
         if verbose > 0:
@@ -75,8 +154,28 @@ def get_business_reviews(id, reviews, max_reviews, verbose=0):
             raise Exception()
 
 
-def get_reviews(business_location, num_reviews, verbose=0):
-    """TODO"""
+def get_reviews(location, num_reviews, collect_all=False, verbose=0):
+    """
+    This function organize the collect of the reviews.
+
+    Parameters
+    ----------
+    location: str
+        The geographic area to be used when searching for businesses
+    num_reviews: int
+        The number of reviews to collect
+    collect_all: bool
+        Whether or not the script should collet all ratings
+    verbose: int
+        0 -> Silent / Progress bar
+        1 -> Infos about the downloaded reviews
+        2 -> HTTP status
+
+    Returns
+    -------
+    list:
+        The list of the reviews (text + rating)
+    """
 
     business_ids = []
     business_offset = 0
@@ -85,7 +184,7 @@ def get_reviews(business_location, num_reviews, verbose=0):
 
     while len(reviews) <= num_reviews:
         request, business_offset = get_business(
-            business_location,
+            location,
             limit=business_limit,
             offset=business_offset,
             verbose=verbose,
@@ -94,9 +193,9 @@ def get_reviews(business_location, num_reviews, verbose=0):
             get_business_id(request, business_limit, business_ids, verbose)
 
             for business_id in business_ids[
-                business_offset - business_limit : business_offset
+                business_offset - business_limit: business_offset
             ]:
-                get_business_reviews(business_id, reviews, num_reviews, verbose=verbose)
+                get_business_reviews(business_id, reviews, num_reviews, collect_all, verbose=verbose)
 
         except Exception:
             break
@@ -107,52 +206,21 @@ def get_reviews(business_location, num_reviews, verbose=0):
     return reviews
 
 
-# def main(location=None, num_reviews=None, save_path=None, verbose=None):
-def main(**kwargs):
+##################################################
+# Generic functions | main + arguments
+##################################################
 
-    if kwargs["location"] is None:
-        location = "France"
-    else:
-        location = kwargs["location"]
+def get_arguments():
+    """
+    Initialize the command line arguments and return
+    the collected values if any...
 
-    if kwargs["num_reviews"] is None:
-        num_reviews = 200
-    else:
-        num_reviews = int(kwargs["num_reviews"])
-
-    if kwargs["save_path"] is None:
-        save_path = os.path.join("data", "api_export.csv")
-    else:
-        save_path = kwargs["save_path"]
-
-    if kwargs["verbose"] is None:
-        verbose = 0
-    else:
-        verbose = int(kwargs["verbose"])
-
-    print(
-        f"location:{location}, num_reviews:{num_reviews}, save_path:{save_path}, verbose:{verbose}\n"
-    )
-
-    if verbose == 0:
-        progress_monitor.items_init(num_reviews, "Connecting to API...")
-
-    reviews = get_reviews(location, num_reviews, verbose=verbose)
-    reviews_df = pd.DataFrame(reviews)
-    reviews_df.to_csv(save_path, index=False)
-
-    terminal_size = os.get_terminal_size()
-    size = terminal_size.columns - 1
-    print(
-        "\n"
-        + f" Exporting dataset of shape {reviews_df.shape} to {save_path} ".center(
-            size, "*"[:size]
-        ),
-        "\n",
-    )
-
-
-if __name__ == "__main__":
+    Returns
+    -------
+    dictionary:
+        A dictionary containing the provided arguments
+        along with their provided values
+    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -179,7 +247,71 @@ if __name__ == "__main__":
         # action="store_true",
         help="The verbosity level (0:Nothing | 1:Reviews | 2:+Business_ids | 3:+HTTP status",
     )
+    parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        help="If this argument is provided, the script will collect all ratings instead of just 1 and 2",
+    )
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+def main(**kwargs):
+    """
+    Verify the provided arguments, then use them to gather the reviews
+    and finally save the reviews in a file.
+
+    Parameters
+    ----------
+    kwargs: dictionary
+        A dictionary containing the provided arguments
+        along with their provided values
+    """
+
+    if kwargs["location"] is None:
+        location = "France"
+    else:
+        location = kwargs["location"]
+
+    if kwargs["num_reviews"] is None:
+        num_reviews = 200
+    else:
+        num_reviews = int(kwargs["num_reviews"])
+
+    if kwargs["save_path"] is None:
+        save_path = os.path.join("data", "api_export.csv")
+    else:
+        save_path = kwargs["save_path"]
+
+    if kwargs["verbose"] is None:
+        verbose = 0
+    else:
+        verbose = int(kwargs["verbose"])
+
+
+    print(
+        f"location:{location}, num_reviews:{num_reviews}, save_path:{save_path}, verbose:{verbose}\n"
+    )
+
+    if verbose == 0:
+        progress_monitor.items_init(num_reviews, "Connecting to API...")
+
+    reviews = get_reviews(location, num_reviews, kwargs['all'], verbose=verbose)
+    reviews_df = pd.DataFrame(reviews)
+    reviews_df.to_csv(save_path, index=False)
+
+    terminal_size = os.get_terminal_size()
+    size = terminal_size.columns - 1
+    print(
+        "\n"
+        + f" Exporting dataset of shape {reviews_df.shape} to {save_path} ".center(
+            size, "*"[:size]
+        ),
+        "\n",
+    )
+
+
+if __name__ == "__main__":
+    args = get_arguments()
     main(**vars(args))
